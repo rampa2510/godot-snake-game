98a4e16bae62e6dcecde27421e00f302f46da946
ram pandey <rampandey@rams-MacBook-Pro.local>
2024-09-24 20:39:00 +0530

    feat: made snake game movements

--- Scenes/snake_face.gd
+++ Scenes/snake_face.gd
@@ -1,42 +1,30 @@
 extends Area2D
 
-var speed:=600
+var speed := 600
+var score := 0
+var direction := Vector2.ZERO
 
-var steering_factor := 5
-
-var direction := Vector2(0,0)
-var velocity := Vector2(0,0)
-var desired_velocity := Vector2(0,0)
-var steering_velocity := Vector2(0,0)
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	area_entered.connect(_on_area_entered)
 
+func _on_area_entered(area_that_entered: Area2D):
+	print(area_that_entered.is_in_group("good_apple"))
 
 func _process(delta: float) -> void:
+	# Handle input
+	if Input.is_action_just_pressed("move_left"):
+		direction = Vector2.LEFT
+	elif Input.is_action_just_pressed("move_right"):
+		direction = Vector2.RIGHT
+	elif Input.is_action_just_pressed("move_up"):
+		direction = Vector2.UP
+	elif Input.is_action_just_pressed("move_down"):
+		direction = Vector2.DOWN
 	
+	position += direction * speed * delta*0.5
 	wrap_snake_head()
-	
-	direction.x = Input.get_axis("move_left","move_right")
-	direction.y = Input.get_axis("move_up","move_down")
-	
-	if direction.length() > 1:
-		direction = direction.normalized()
-		
-	
-	desired_velocity = direction * speed
-	steering_velocity = desired_velocity - velocity
-		
-	velocity += steering_velocity * steering_factor *delta
-	position += velocity * delta
-	
-	
+
 func wrap_snake_head() -> void:
 	var viewport_size := get_viewport_rect().size
-
 	position.x = wrapf(position.x, 0, viewport_size.x)
 	position.y = wrapf(position.y, 0, viewport_size.y)
-
-	
